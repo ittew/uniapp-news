602e0979177234a21d955207dc27a60acf93964b
ittew <873077939@qq.com>
2020-08-16 16:19:06 +0800

    fix: 完成导航栏即tab数据获取

---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="content">
+    <navbar />
+    <tab v-if="tabList" :list="tabList" />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello11',
+      tabList: []
+    }
+  },
+  onLoad() {
+    this.getLabel()
+  },
+  methods: {
+    getLabel(){
+      // 调用云函数方法
+      uniCloud.callFunction({
+        name:"get_label"
+      }).then(res=>{
+        const {result} = res
+        this.tabList = result.data
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
